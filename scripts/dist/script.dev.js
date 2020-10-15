9fad5b89a7bdc047c8f7cbb2ca691e2e4c6fa76a
Dani <dustyglass@gmail.com>
2020-10-15 19:21:18 -0300

    second commit

--- scripts/dist/script.dev.js
+++ scripts/dist/script.dev.js
@@ -1,9 +1,11 @@
 "use strict";
 
-function _readOnlyError(name) { throw new Error("\"" + name + "\" is read-only"); }
-
 var apiKey = "vWxLs91pSMGABQsCprkgIF3EsXy3WnZA";
 var offset = 0;
+var counter = 12;
+var popId = 1;
+var searchArea = document.getElementById('see-more-area');
+searchArea.innerHTML = "";
 
 var getTrending = function getTrending() {
   url = "https://api.giphy.com/v1/gifs/trending?api_key=".concat(apiKey, "&limit=3&offset=").concat(offset);
@@ -16,10 +18,38 @@ var getTrending = function getTrending() {
 
 var renderTrending = function renderTrending(gif) {
   console.log(gif);
-  var trending = document.getElementById("trending-carrousel");
-  var p = document.createElement('p');
-  p.innerHTML = "<img src=".concat(gif.images.downsized.url, " class=\"trending-gif\" ></img>");
-  trending.appendChild(p);
+  return new Promise(function (resolve, reject) {
+    var trending = document.getElementById("trending-carrousel");
+    var image = document.createElement('p');
+    var favourite = document.createElement('p');
+    var download = document.createElement('p');
+    var maximize = document.createElement('p');
+    var popup = document.createElement('div');
+    image.innerHTML = "<img src=".concat(gif.images.downsized.url, " class=\"trending-gif\" alt=\"gif\" >");
+    popup.innerHTML = "<div class=\"popup-wrapper\" id=\"popup-wrapper".concat(popId, "\"><div class=\"popup\"><div class=\"popup-close\" id=\"popup-close").concat(popId, "\">x</div> <div class=\"popup-content\" id=\"popup-content\"><img src=").concat(gif.images.downsized.url, " class=\"pop-up-image\" alt=\"gif\" ></img></div></div></div>");
+    favourite.innerHTML = "<img src= \"images/icon-fav-hover.svg\" alt=\"favourite\" class=\"favourite\"/>";
+    download.innerHTML = "<img src=\"images/icon-download-hover.svg\" alt=\"download\" class=\"download\" />";
+    maximize.innerHTML = "<img src =\"images/icon-max-hover.svg\" alt=\"maximize\" class=\"max\" />";
+    trending.appendChild(popup);
+    trending.appendChild(image);
+    trending.appendChild(favourite);
+    trending.appendChild(download);
+    trending.appendChild(maximize);
+    var id = popId;
+    maximize.addEventListener("click", function () {
+      return maximizeImage(id);
+    });
+  }).then(popId += 1);
+};
+
+var maximizeImage = function maximizeImage(id) {
+  console.log(id);
+  var popupWrapper = document.getElementById("popup-wrapper".concat(id));
+  popupWrapper.style.display = 'block';
+  var popupClose = document.getElementById("popup-close".concat(id));
+  popupClose.addEventListener("click", function () {
+    return popupWrapper.style.display = "none";
+  });
 };
 
 var hoverCreate = function hoverCreate() {
@@ -55,7 +85,7 @@ var rightOff = function rightOff() {
 var carrouselRight = function carrouselRight() {
   var trending = document.getElementById("trending-carrousel");
   trending.innerHTML = "";
-  offset += (_readOnlyError("offset"), 3);
+  offset += 3;
   url = "https://api.giphy.com/v1/gifs/trending?api_key=".concat(apiKey, "&limit=3&offset=").concat(offset);
   fetch(url).then(function (response) {
     return response.json();
@@ -65,7 +95,7 @@ var carrouselRight = function carrouselRight() {
 };
 
 var carrouselLeft = function carrouselLeft() {
-  offset -= (_readOnlyError("offset"), 3);
+  offset -= 3;
   var trending = document.getElementById("trending-carrousel");
   trending.innerHTML = "";
   url = "https://api.giphy.com/v1/gifs/trending?api_key=".concat(apiKey, "&limit=3&offset=").concat(offset);
@@ -76,24 +106,246 @@ var carrouselLeft = function carrouselLeft() {
   });
 };
 
+var clear = function clear() {
+  var trendingHeader = document.getElementById('trending-topics-header');
+  var trendingTopics = document.getElementById('trending-topics');
+  var searchResults = document.getElementById('search-results');
+  var seeMore = document.getElementById('see-more-area');
+  trendingHeader.innerHTML = '';
+  trendingTopics.innerHTML = '';
+  searchResults.innerHTML = '';
+  seeMore.innerHTML = '';
+};
+
+var input = document.getElementById("searchInput");
+
+if (input.value.length !== 0) {
+  input.addEventListener("click", function () {
+    return search();
+  });
+}
+
 var search = function search() {
-  var searchArea = document.getElementById("search-area");
-  searchArea.innerHTML = '';
+  clear();
   var input = document.getElementById("searchInput");
   var query = input.value;
-  url = "https://api.giphy.com/v1/gifs/search?api_key=vWxLs91pSMGABQsCprkgIF3EsXy3WnZA&q=".concat(query, "&limit=12&offset=0");
+  url = "https://api.giphy.com/v1/gifs/search?api_key=".concat(apiKey, "&q=").concat(query, "&limit=").concat(counter, "&offset=0");
   fetch(url).then(function (response) {
     return response.json();
-  }).then(function (gifs) {
-    return gifs.data.forEach(renderSearch);
+  }).then(function (response) {
+    return showResults(response);
+  }).then(function (response) {
+    return seeMore(response);
+  }).then(function (response) {
+    return clearSeeMore();
   });
 };
 
-var renderSearch = function renderSearch(gif) {
+var clearSeeMore = function clearSeeMore() {
   var searchResults = document.getElementById("search-results");
-  console.log(searchResults); // const p = document.createElement('p')
-  // p.innerHTML = `<img src=${gif.images.downsized.url} class="searched-gif" ></img>`
-  // searchResults.appendChild(p)
+
+  if (!searchResults.hasChildNodes()) {
+    var _searchArea = document.getElementById('see-more');
+
+    _searchArea.innerHTML = "";
+  }
 };
 
+var seeMore = function seeMore() {
+  var searchArea = document.getElementById('see-more-area');
+  searchArea.innerHTML = "";
+  var img = document.createElement('img');
+  img.src = "images/CTA-ver-mas.svg";
+  img.id = "see-more";
+  img.className = "see-more";
+  searchArea.appendChild(img);
+  img.addEventListener("click", function () {
+    counter += 12;
+    search();
+  });
+  img.addEventListener("mouseover", function () {
+    return img.src = "images/CTA-ver-mas-hover.svg";
+  });
+  img.addEventListener("mouseout", function () {
+    return img.src = "images/CTA-ver-mas.svg";
+  });
+};
+
+var showResults = function showResults(response) {
+  var input = document.getElementById("searchInput");
+
+  if (response.data.length == 0 && input.value.length !== 0) {
+    var searchResults = document.getElementById("search-results");
+    searchResults.innerHTML = '';
+    var div = document.createElement('div');
+    var p1 = document.createElement('p');
+    var p2 = document.createElement('p');
+    var p3 = document.createElement('p');
+    div.className = "no-results";
+    p1.innerText = input.value;
+    p1.className = "no-results-title";
+    p2.innerHTML = "<img src=\"images/icon-busqueda-sin-resultado.svg\" class=\"no-results-image\" ></img>";
+    p3.innerText = "Intenta con otra búsqueda";
+    p3.className = "no-results-text";
+    searchResults.appendChild(div);
+    div.appendChild(p1);
+    div.appendChild(p2);
+    div.appendChild(p3);
+  } else {
+    clear();
+    response.data.forEach(renderSearch);
+  }
+};
+
+var renderSearch = function renderSearch(gif) {
+  return new Promise(function (resolve, reject) {
+    var searchResults = document.getElementById("search-results");
+    var image = document.createElement('p');
+    var favourite = document.createElement('p');
+    var download = document.createElement('p');
+    var maximize = document.createElement('p');
+    var popup = document.createElement('div');
+    image.innerHTML = "<img src=".concat(gif.images.downsized.url, " class=\"trending-gif\" alt=\"gif\" >");
+    popup.innerHTML = "<div class=\"popup-wrapper\" id=\"popup-wrapper".concat(popId, "\"><div class=\"popup\"><div class=\"popup-close\" id=\"popup-close").concat(popId, "\">x</div> <div class=\"popup-content\" id=\"popup-content\"><img src=").concat(gif.images.downsized.url, " class=\"pop-up-image\" alt=\"gif\" ></img></div></div></div>");
+    favourite.innerHTML = "<img src= \"images/icon-fav-hover.svg\" alt=\"favourite\" class=\"favourite\"/>";
+    download.innerHTML = "<img src=\"images/icon-download-hover.svg\" alt=\"download\" class=\"download\" />";
+    maximize.innerHTML = "<img src =\"images/icon-max-hover.svg\" alt=\"maximize\" class=\"max\" />";
+    searchResults.appendChild(popup);
+    searchResults.appendChild(image);
+    searchResults.appendChild(favourite);
+    searchResults.appendChild(download);
+    searchResults.appendChild(maximize);
+    var id = popId;
+    maximize.addEventListener("click", function () {
+      return maximizeImage(id);
+    });
+  }).then(popId += 1);
+};
+
+var searchSuggestions = function searchSuggestions() {
+  clear();
+  var searchSuggestions = document.getElementById("search-suggestions");
+  var input = document.getElementById("searchInput");
+  var query = input.value;
+  url = "https://api.giphy.com/v1/gifs/search/tags?api_key=".concat(apiKey, "&q=").concat(query, "&limit=5");
+  fetch(url).then(function (response) {
+    return response.json();
+  }).then(function (topics) {
+    return topics.data.forEach(renderSuggestion);
+  });
+  searchSuggestions.innerHTML = '';
+};
+
+var searchForSuggestion = function searchForSuggestion(query) {
+  clear();
+  url = "https://api.giphy.com/v1/gifs/search?api_key=".concat(apiKey, "&q=").concat(query, "&limit=").concat(counter, "&offset=0");
+  fetch(url).then(function (response) {
+    return response.json();
+  }).then(function (response) {
+    return showResults(response);
+  }).then(function (response) {
+    return seeMoreSuggested(query);
+  }).then(function (response) {
+    return clearSeeMore();
+  });
+};
+
+var seeMoreSuggested = function seeMoreSuggested(query) {
+  var searchArea = document.getElementById('see-more-area');
+  searchArea.innerHTML = "";
+  var img = document.createElement('img');
+  img.src = "images/CTA-ver-mas.svg";
+  img.id = "see-more";
+  img.className = "see-more";
+  searchArea.appendChild(img);
+  img.addEventListener("click", function () {
+    counter += 12;
+    searchForSuggestion(query);
+  });
+  img.addEventListener("mouseover", function () {
+    return img.src = "images/CTA-ver-mas-hover.svg";
+  });
+  img.addEventListener("mouseout", function () {
+    return img.src = "images/CTA-ver-mas.svg";
+  });
+};
+
+var renderSuggestion = function renderSuggestion(topic) {
+  var input = document.getElementById("searchInput");
+  var searchSuggestions = document.getElementById("search-suggestions");
+  var p = document.createElement('p');
+  p.innerHTML = "<img src=\"images/icon-search.svg\" alt=\"search\"/>".concat(topic.name);
+  p.className = "topic";
+  searchSuggestions.appendChild(p);
+  p.addEventListener('click', function () {
+    return searchForSuggestion("".concat(topic.name));
+  });
+  input.addEventListener("keydown", function (event) {
+    return keyCheck(event);
+  });
+  input.addEventListener("click", function (event) {
+    return clearResults(event);
+  });
+};
+
+var keyCheck = function keyCheck(event) {
+  var input = document.getElementById("searchInput");
+  input.style.backgroundImage = "url('images/close.svg')";
+
+  if (event.keyCode == 8 || event.keyCode == 46) {
+    var _searchArea2 = document.getElementById('see-more');
+
+    _searchArea2.innerHTML = "";
+
+    var _searchSuggestions = document.getElementById("search-suggestions");
+
+    _searchSuggestions.innerHTML = '';
+    var p1 = document.createElement('p');
+    p1.innerText = "Trending:";
+    p1.className = "trending-topics-header";
+    var p2 = document.createElement('p');
+    p2.innerHTML = "Reactions, Entertainment, Sports,Stickers, Artists";
+    p2.className = "trending-topics";
+
+    _searchSuggestions.appendChild(p1);
+
+    _searchSuggestions.appendChild(p2);
+  }
+
+  if (event.keyCode == 13) {
+    search();
+  }
+};
+
+var clearResults = function clearResults(event) {
+  var searchSuggestions = document.getElementById("search-suggestions");
+  searchSuggestions.innerHTML = '';
+  var input = document.getElementById("searchInput");
+  input.value = '';
+  input.style.backgroundImage = "url('images/icon-search.svg')";
+  var seeMore = document.getElementById('see-more');
+  seeMore.innerHTML = '';
+};
+
+var facebook = document.getElementById("facebook");
+var twitter = document.getElementById("twitter");
+var instagram = document.getElementById("instagram");
+facebook.addEventListener("mouseover", function () {
+  return facebook.src = "images/icon_facebook_hover.svg";
+});
+facebook.addEventListener("mouseout", function () {
+  return facebook.src = "images/icon_facebook.svg";
+});
+twitter.addEventListener("mouseover", function () {
+  return twitter.src = "images/icon-twitter-hover.svg";
+});
+twitter.addEventListener("mouseout", function () {
+  return twitter.src = "images/icon-twitter.svg";
+});
+instagram.addEventListener("mouseover", function () {
+  return instagram.src = "images/icon_instagram-hover.svg";
+});
+instagram.addEventListener("mouseout", function () {
+  return instagram.src = "images/icon_instagram.svg";
+});
 getTrending();
--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,52 @@
 const apiKey= "vWxLs91pSMGABQsCprkgIF3EsXy3WnZA"
-const offset = 0;
+let offset = 0;
+let counter = 12;
+let popId = 1;
+
+const searchArea = document.getElementById('see-more-area')
+searchArea.innerHTML = ""
 
 const getTrending = () => {
  url= `https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&limit=3&offset=${offset}`
  fetch(url)
  .then(response => response.json())
  .then(response => response.data.forEach(renderTrending))
-
 }
 
 const renderTrending = (gif) => {
     console.log(gif)
+return new Promise((resolve, reject) => {
     const trending = document.getElementById("trending-carrousel")
-        const p = document.createElement('p')
-        p.innerHTML = `<img src=${gif.images.downsized.url} class="trending-gif" ></img>`
-        trending.appendChild(p)
+    const image = document.createElement('p')
+    const favourite = document.createElement('p')
+    const download = document.createElement('p')
+    const maximize = document.createElement('p') 
+    const popup = document.createElement('div')
+    image.innerHTML = `<img src=${gif.images.downsized.url} class="trending-gif" alt="gif" >`  
+    popup.innerHTML = `<div class="popup-wrapper" id="popup-wrapper${popId}"><div class="popup"><div class="popup-close" id="popup-close${popId}">x</div> <div class="popup-content" id="popup-content"><img src=${gif.images.downsized.url} class="pop-up-image" alt="gif" ></img></div></div></div>`
+    favourite.innerHTML = `<img src= "images/icon-fav-hover.svg" alt="favourite" class="favourite"/>`
+    download.innerHTML = `<img src="images/icon-download-hover.svg" alt="download" class="download" />`
+    maximize.innerHTML = `<img src ="images/icon-max-hover.svg" alt="maximize" class="max" />`
+    trending.appendChild(popup)
+    trending.appendChild(image)
+    trending.appendChild(favourite)
+    trending.appendChild(download)
+    trending.appendChild(maximize)
+    const id=popId
+    maximize.addEventListener("click", () => maximizeImage(id))
+        
+})
+    .then(popId += 1)
+    
+}
+
+
+const maximizeImage = (id) => {
+    (console.log(id))
+    const popupWrapper = document.getElementById(`popup-wrapper${id}`)
+    popupWrapper.style.display = 'block'
+    const popupClose = document.getElementById(`popup-close${id}`)
+    popupClose.addEventListener("click", () => popupWrapper.style.display ="none")
 }
 
 const hoverCreate = () => {
@@ -49,7 +81,7 @@ const rightOff = () => {
 
 const carrouselRight = () => {
     const trending = document.getElementById("trending-carrousel")
-    trending.innerHTML="";
+   trending.innerHTML="";
     offset += 3;
     url= `https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&limit=3&offset=${offset}`
     fetch(url)
@@ -67,23 +99,210 @@ const carrouselLeft = () => {
     .then(response => response.data.forEach(renderTrending))
 }
 
+const clear = () => {
+    const trendingHeader = document.getElementById('trending-topics-header')
+    const trendingTopics = document.getElementById('trending-topics')
+    const searchResults = document.getElementById('search-results')
+    const seeMore = document.getElementById('see-more-area')
+    trendingHeader.innerHTML = ''
+    trendingTopics.innerHTML = ''
+    searchResults.innerHTML = ''
+    seeMore.innerHTML = ''
+}
+const input = document.getElementById("searchInput")
+if(input.value.length !== 0){
+    input.addEventListener("click", () => search())
+}
+
+
 const search = () => {
-    const searchArea = document.getElementById("search-area")
-    searchArea.innerHTML = ''
+    clear()
     const input = document.getElementById("searchInput")
     const query = input.value
-    url= `https://api.giphy.com/v1/gifs/search?api_key=vWxLs91pSMGABQsCprkgIF3EsXy3WnZA&q=${query}&limit=12&offset=0`
+    url= `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${query}&limit=${counter}&offset=0`
     fetch(url)
     .then(response => response.json())
-    .then(gifs => gifs.data.forEach(renderSearch))    
+    .then(response => showResults(response))
+    .then(response => seeMore(response) )
+    .then(response => clearSeeMore())
+}    
+
+const clearSeeMore = () => {
+    const searchResults = document.getElementById("search-results")
+    if(!searchResults.hasChildNodes()){
+        const searchArea = document.getElementById('see-more')
+        searchArea.innerHTML = ""
+    }
+    
+}
+
+const seeMore = () => {
+    const searchArea = document.getElementById('see-more-area')
+    searchArea.innerHTML = ""
+    const img = document.createElement('img')
+    img.src = "images/CTA-ver-mas.svg"
+    img.id = "see-more"
+    img.className = "see-more"
+    searchArea.appendChild(img)
+    img.addEventListener("click", () => {
+        counter += 12
+        search()
+    })
+    img.addEventListener("mouseover", () => img.src = "images/CTA-ver-mas-hover.svg")
+    img.addEventListener("mouseout", () => img.src = "images/CTA-ver-mas.svg")
+}
+
+
+
+const showResults = (response) => {
+    const input = document.getElementById("searchInput")
+    if(response.data.length == 0 && input.value.length !==0 ){
+        const searchResults = document.getElementById("search-results")
+        searchResults.innerHTML = ''
+        const div = document.createElement('div')
+        const p1 = document.createElement('p')
+        const p2 = document.createElement('p')
+        const p3 = document.createElement('p')
+        div.className = "no-results"
+        p1.innerText = input.value
+        p1.className = "no-results-title"
+        p2.innerHTML = `<img src="images/icon-busqueda-sin-resultado.svg" class="no-results-image" ></img>`
+        p3.innerText = "Intenta con otra búsqueda"
+        p3.className = "no-results-text"
+        searchResults.appendChild(div)
+        div.appendChild(p1)
+        div.appendChild(p2)
+        div.appendChild(p3)
+        
+    } else{
+        clear()
+        response.data.forEach(renderSearch)
+    } 
 }
 
 const renderSearch = (gif) => {
-    const searchResults = document.getElementById("search-results")
-    console.log(searchResults)
-    // const p = document.createElement('p')
-    // p.innerHTML = `<img src=${gif.images.downsized.url} class="searched-gif" ></img>`
-    // searchResults.appendChild(p)
+    return new Promise((resolve, reject) => {
+        const searchResults = document.getElementById("search-results")
+        const image = document.createElement('p')
+        const favourite = document.createElement('p')
+        const download = document.createElement('p')
+        const maximize = document.createElement('p') 
+        const popup = document.createElement('div')
+        image.innerHTML = `<img src=${gif.images.downsized.url} class="trending-gif" alt="gif" >`  
+        popup.innerHTML = `<div class="popup-wrapper" id="popup-wrapper${popId}"><div class="popup"><div class="popup-close" id="popup-close${popId}">x</div> <div class="popup-content" id="popup-content"><img src=${gif.images.downsized.url} class="pop-up-image" alt="gif" ></img></div></div></div>`
+        favourite.innerHTML = `<img src= "images/icon-fav-hover.svg" alt="favourite" class="favourite"/>`
+        download.innerHTML = `<img src="images/icon-download-hover.svg" alt="download" class="download" />`
+        maximize.innerHTML = `<img src ="images/icon-max-hover.svg" alt="maximize" class="max" />`
+        searchResults.appendChild(popup)
+        searchResults.appendChild(image)
+        searchResults.appendChild(favourite)
+        searchResults.appendChild(download)
+        searchResults.appendChild(maximize)
+        const id=popId
+        maximize.addEventListener("click", () => maximizeImage(id))
+            
+    })
+        .then(popId += 1)
+        
 }
+
+const searchSuggestions = () => {
+    clear()
+    const searchSuggestions = document.getElementById("search-suggestions")
+    const input = document.getElementById("searchInput")
+    const query = input.value
+    url= `https://api.giphy.com/v1/gifs/search/tags?api_key=${apiKey}&q=${query}&limit=5`
+    fetch(url)
+    .then(response => response.json())
+    .then(topics => topics.data.forEach(renderSuggestion))
+    searchSuggestions.innerHTML=''
+}
+
+const searchForSuggestion = (query) => {
+    clear()
+    url= `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${query}&limit=${counter}&offset=0`
+    fetch(url)
+    .then(response => response.json())
+    .then(response => showResults(response))
+    .then(response => seeMoreSuggested(query))
+    .then(response => clearSeeMore())
+}
+
+const seeMoreSuggested = (query) => {
+    const searchArea = document.getElementById('see-more-area')
+    searchArea.innerHTML = ""
+    const img = document.createElement('img')
+    img.src = "images/CTA-ver-mas.svg"
+    img.id = "see-more"
+    img.className = "see-more"
+    searchArea.appendChild(img)
+    img.addEventListener("click", () => {
+        counter += 12
+        searchForSuggestion(query)
+    })
+
+    img.addEventListener("mouseover", () => img.src = "images/CTA-ver-mas-hover.svg")
+    img.addEventListener("mouseout", () => img.src = "images/CTA-ver-mas.svg")
+}
+
+const renderSuggestion = (topic) => {
+    const input = document.getElementById("searchInput")
+    const searchSuggestions = document.getElementById("search-suggestions")
+    const p = document.createElement('p')
+    p.innerHTML = `<img src="images/icon-search.svg" alt="search"/>${topic.name}`
+    p.className = "topic"
+    searchSuggestions.appendChild(p)
+    p.addEventListener('click',() => searchForSuggestion(`${topic.name}`))
+    input.addEventListener("keydown", event => keyCheck(event))
+    input.addEventListener("click", event => clearResults(event))
+}
+
+const keyCheck = (event) => {
+    const input = document.getElementById("searchInput")
+    input.style.backgroundImage = "url('images/close.svg')";
+
+    if(event.keyCode == 8 || event.keyCode == 46){
+        const searchArea = document.getElementById('see-more')
+        searchArea.innerHTML = ""
+        const searchSuggestions = document.getElementById("search-suggestions")
+        searchSuggestions.innerHTML = ''
+        const p1 = document.createElement('p')
+        p1.innerText = "Trending:"
+        p1.className = "trending-topics-header"
+        const p2 = document.createElement('p')
+        p2.innerHTML = "Reactions, Entertainment, Sports,Stickers, Artists"
+        p2.className = "trending-topics"
+        searchSuggestions.appendChild(p1)
+        searchSuggestions.appendChild(p2)
+    }
+
+    if(event.keyCode == 13){
+        search()
+    }
+}
+
+const clearResults = (event) => {
+    const searchSuggestions = document.getElementById("search-suggestions")
+    searchSuggestions.innerHTML = ''
+    const input = document.getElementById("searchInput")
+    input.value = ''
+    input.style.backgroundImage = "url('images/icon-search.svg')";
+    const seeMore = document.getElementById('see-more')
+    seeMore.innerHTML = ''
+}
+
+const facebook = document.getElementById("facebook");
+const twitter = document.getElementById("twitter");
+const instagram = document.getElementById("instagram");
+
+facebook.addEventListener("mouseover", () => facebook.src = "images/icon_facebook_hover.svg")
+facebook.addEventListener("mouseout", () => facebook.src = "images/icon_facebook.svg")
+twitter.addEventListener("mouseover", () => twitter.src = "images/icon-twitter-hover.svg")
+twitter.addEventListener("mouseout", () => twitter.src = "images/icon-twitter.svg")
+instagram.addEventListener("mouseover", () => instagram.src = "images/icon_instagram-hover.svg")
+instagram.addEventListener("mouseout", () => instagram.src = "images/icon_instagram.svg")
+
+
+
 
 getTrending();
